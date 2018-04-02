2583f354c281cea7ecb9adc44eaf6c16f4a4a0ba
ToshiLock <andrewgill_zch@outlook.com>
2018-04-02 17:08:33 -0600

    wrote and testest nearest()

--- Kdtrees/src/kDTrees/PointST.java
+++ Kdtrees/src/kDTrees/PointST.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.RedBlackBST;
+import edu.princeton.cs.algs4.StdOut;
 
 public class PointST<T> {
 
@@ -70,18 +71,55 @@ public class PointST<T> {
 		return points;
 	}
 	  
-	public Iterable<Point2D> range(RectHV rect)
+	//public Iterable<Point2D> range(RectHV rect)
 	{
 	 // all points that are inside the rectangle 
 	}
 	 
+	// a nearest neighbor to point p; null if the symbol table is empty 
+	
 	public Point2D nearest(Point2D p)
 	{
-	 // a nearest neighbor to point p; null if the symbol table is empty 
+		//error checks
+		if(p == null)
+			throw new java.lang.NullPointerException("the point must not be null");
+		
+		if(isEmpty())
+			return null;
+		
+		//go thorugh all points recording the shortest as you go;
+		// already done empty check so this is safe
+		double shortDistance = symTable.max().distanceTo(p); //something to compare to
+		Point2D shortPoint = symTable.max();
+		
+		int size =  symTable.size();
+		
+		for(int i = 0; i < size ; i++)
+		{
+			if(symTable.select(i).distanceTo(p) < shortDistance)
+			{
+				shortPoint = symTable.select(i);
+				shortDistance = shortPoint.distanceTo(p);
+			}
+		}
+		
+		return shortPoint;
 	}
 	  
 	public static void main(String[] args)    
 	{
-	 // unit testing of the methods (not graded) 
+		// unit testing of the methods (not graded) 
+		PointST<Integer> test = new PointST<Integer>(); 
+		
+		test.put(new Point2D(0,1), 1);
+		test.put(new Point2D(0,3), 2);
+		test.put(new Point2D(3,4), 3);
+		
+		for(Point2D p: test.points())
+		{
+			StdOut.println(p);
+		}
+		
+		StdOut.println(test.nearest(new Point2D(1,2)));
 	}
 }
