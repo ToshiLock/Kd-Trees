f6f08706ff3f4f3c05496bc5da369f616195ad88
ToshiLock <andrewgill_zch@outlook.com>
2018-04-02 17:21:28 -0600

    finised pointST calss now to make next class

--- Kdtrees/src/kDTrees/PointST.java
+++ Kdtrees/src/kDTrees/PointST.java
@@ -10,24 +10,26 @@ public class PointST<T> {
 
 	private RedBlackBST<Point2D, T> symTable;
 	
+
+	// construct an empty symbol table of points
 	public PointST()
 	{
-		// construct an empty symbol table of points
 		symTable = new RedBlackBST<Point2D,T>(); 
 	}
-	 
+
+	// is the symbol table empty? 
 	public boolean isEmpty()
 	{
-		// is the symbol table empty? 
 		return symTable.isEmpty();
 	}
-	  
+
+	// number of points 
 	public int size()
 	{
-		// number of points 
 		return symTable.size();
 	}
-	  
+
+	// associate the value val with point p
 	public void put(Point2D p, T val)
 	{
 		if (p == null)
@@ -35,31 +37,31 @@ public class PointST<T> {
 		if (val == null)
 			throw new java.lang.NullPointerException("the value must not be null");
 		
-		// associate the value val with point p
 		symTable.put(p, val);
 	}
-	  
+
+	// value associated with point p 
 	public T get(Point2D p)
 	{
 		if (p == null)
 			throw new java.lang.NullPointerException("the point must not be null");
 		
-		// value associated with point p 
 		return symTable.get(p);
 	}
-	  
+
+	// does the symbol table contain point p? 
 	public boolean contains(Point2D p)
 	{
 		if (p == null)
 			throw new java.lang.NullPointerException("the point must not be null");
 		
-		// does the symbol table contain point p? 
 		return symTable.contains(p);
 	}
 	  
+
+	// all points in the symbol table
 	public Iterable<Point2D> points()
 	{
-		// all points in the symbol table
 		Queue<Point2D> points = new Queue<Point2D>(); 
 		int size =  symTable.size();
 		
@@ -70,24 +72,37 @@ public class PointST<T> {
 		
 		return points;
 	}
-	  
-	//public Iterable<Point2D> range(RectHV rect)
+
+	// all points that are inside the rectangle 
+	public Iterable<Point2D> range(RectHV rect)
 	{
-	 // all points that are inside the rectangle 
+		if(rect == null)
+			throw new java.lang.NullPointerException("the rectangle must not be null");
+		
+		Queue<Point2D> points = new Queue<Point2D>();
+		int size =  symTable.size();
+		
+		
+		for(int i = 0; i < size ; i++)
+		{
+			if(rect.contains(symTable.select(i)))
+			{
+				points.enqueue(symTable.select(i));
+			}
+		}
+		
+		return points;
 	}
 	 
 	// a nearest neighbor to point p; null if the symbol table is empty 
-	
 	public Point2D nearest(Point2D p)
 	{
-		//error checks
 		if(p == null)
 			throw new java.lang.NullPointerException("the point must not be null");
 		
 		if(isEmpty())
 			return null;
 		
-		//go thorugh all points recording the shortest as you go;
 		// already done empty check so this is safe
 		double shortDistance = symTable.max().distanceTo(p); //something to compare to
 		Point2D shortPoint = symTable.max();
@@ -120,6 +135,9 @@ public class PointST<T> {
 			StdOut.println(p);
 		}
 		
-		StdOut.println(test.nearest(new Point2D(1,2)));
+		for(Point2D p : test.range(new RectHV(0, -1, 1, 4)))
+		{
+			StdOut.println(p);
+		}
 	}
 }
